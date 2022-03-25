e7159297919bc7a6053a05f9f544097651aca8d6
CHEN Roxane <chen.roxane19@gmail.com>
2022-03-25 17:35:34 +0100

    add arduino code

---
+++ arduinocode/test_connection/test_connection.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}void loop() {
+  char incomingByte;
+   // If there is a data stored in the serial receive buffer, read it and print it to the serial port as human-readable ASCII text.
+  if(Serial.available()){  
+    incomingByte = Serial.read();
+    Serial.print(incomingByte);  
+  }
+}
